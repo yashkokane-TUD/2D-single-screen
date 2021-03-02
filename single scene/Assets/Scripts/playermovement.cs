cfa75e51426d4fe7afb39ae4afc82f171e214902
yashkokane-TUD <77935215+yashkokane-TUD@users.noreply.github.com>
2021-03-02 16:13:45 +0000

    added input system

---
+++ single scene/Assets/Scripts/playermovement.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveForce, maxSpeed, jumpForce;
+    [SerializeField] private Collider2D groundCheck;
+    [SerializeField] private LayerMask groundLayers;
+
+    private float moveDir;
+    private Rigidbody2D myRB;
+    private bool canJump;
+
+    private void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        var moveAxis = Vector2.right * moveDir;
+
+        if (Mathf.Abs(myRB.velocity.x) < maxSpeed)
+        {
+            myRB.AddForce(moveAxis * moveForce, ForceMode2D.Force);
+        }
+
+        if (groundCheck.IsTouchingLayers(groundLayers))
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveDir = context.ReadValue<float>();
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (canJump && context.started)
+        {
+            myRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            canJump = false;
+        }
+
+        if (context.canceled && myRB.velocity.y > 0)
+        {
+            myRB.velocity = new Vector2(myRB.velocity.x, 0f);
+        }
+    }
+
+}
