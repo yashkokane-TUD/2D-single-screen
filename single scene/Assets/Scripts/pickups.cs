f5f97dbee2abd4e156730c9828284e2316e15c26
yashkokane-TUD <77935215+yashkokane-TUD@users.noreply.github.com>
2021-03-15 18:26:09 +0000

    script update

--- single scene/Assets/Scripts/pickups.cs
+++ single scene/Assets/Scripts/pickups.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class pickups : MonoBehaviour
 {
-    private int bonus = 100;
-    // Start is called before the first frame update
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
